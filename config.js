370353a58b3b1385b14b0cbd4d72b0c35194587b
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-06-16 17:48:02 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+var api = "https://api.npool.pw/rto";
+
+var poolHost = "ca.npool.pw";
+
+var blockExplorer = "http://explorer.arto.cash/";
+
+var cryptonatorWidget = ["BTC-USD", "RTO-BTC"];
+
+var blockchainExplorer = "http://explorer.arto.cash/?hash={id}#blockchain_block";
+
+var transactionExplorer = "http://explorer.arto.cash/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default-theme.css";
+
+var networkStat = [
+        ["npool.pw/arto", "https://api.npool.pw/rto"],
+        ["pool.arto.cash", "http://pool.arto.cash:8117"],
+        ["arto.fatpanda.club", "http://arto.cryptonight.me:8024"],
+        ["arto.route199.com", "http://arto.route199.com:8117"],
+        ["arto.cryptoisme.com", "http://arto.cryptoisme.com:9119"],
+        ["newpool.pw/arto", "http://mine.newpool.pw:8124"],
+        ["cryptoknight.cc/arto", "https://cryptoknight.cc/rpc/arto"]
+];
